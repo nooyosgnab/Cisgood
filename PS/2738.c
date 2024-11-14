243192a4d3a73d70ca455f065927bde3f8601a45
Soyoon Bang <127003632+nooyosgnab@users.noreply.github.com>
2024-11-15 07:57:22 +0900

    Create 2738.c

---
+++ PS/2738.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    int A[N][M];
+    int B[N][M];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            printf("%d ", A[i][j] + B[i][j]);
+        }
+        printf("\n");
+    }
+}
